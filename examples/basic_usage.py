a134af339e1c97c241fc2e035995c831682b0a19
Paul Simmering <paul.simmering@gmail.com>
2023-08-04 12:02:02 +0200

    Rewrite basic use script

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -3,19 +3,13 @@ import texttunnel.chat
 import texttunnel.models
 import texttunnel.processor
 
-messages = texttunnel.chat.Chat(
-    [
-        texttunnel.chat.ChatMessage(
-            role="system",
-            content="You are a sentiment analysis expert.",
-        ),
-        texttunnel.chat.ChatMessage(
-            role="user",
-            content="Classify the following statement as positive or negative: I love sunshine.",
-        ),
-    ]
-)
+# Texts that we'd like to know the sentiment of
+input_texts = [
+    "I love sunshine",
+    "I don't like rain",
+]
 
+# Sentiment analysis function
 function = {
     "name": "function_name",
     "parameters": {
@@ -26,16 +20,48 @@ function = {
     },
 }
 
-request = texttunnel.chat.ChatCompletionRequest(
-    model=texttunnel.models.GPT_3_5_TURBO,
-    chat=messages,
-    function=function,
-)
+requests = []
 
-requests = [request, request, request]
+for text in input_texts:
+    messages = texttunnel.chat.Chat(
+        [
+            texttunnel.chat.ChatMessage(
+                role="system",
+                content="You are a sentiment analysis expert.",
+            ),
+            texttunnel.chat.ChatMessage(
+                role="user",
+                content=f"Classify the following statement as positive or negative: {text}",
+            ),
+        ]
+    )
 
+    request = texttunnel.chat.ChatCompletionRequest(
+        model=texttunnel.models.GPT_3_5_TURBO,
+        chat=messages,
+        function=function,
+    )
+
+    requests.append(request)
+
+# %%
+# Estimate the cost of the requests
+cost_usd = sum([r.estimate_input_cost_usd() for r in requests])
+print(f"Estimated cost of input tokens: ${cost_usd:.4f}")
+
+# %%
 # Requires that the OPENAI_API_KEY environment variable is set.
-texttunnel.processor.process_api_requests(
+responses = texttunnel.processor.process_api_requests(
     requests=requests,
     save_filepath="output.jsonl",
+    keep_file=False,
+    logging_level=50 # only log errors
 )
+
+# %%
+results = texttunnel.processor.parse_responses(responses=responses)
+
+for text, sentiment in zip(input_texts, results):
+    print(f"{text}: {sentiment}")
+
+# %%
