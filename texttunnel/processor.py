9d3aefe2bee09a2e9d43c41673402a085368a155
Paul Simmering <paul.simmering@gmail.com>
2023-08-04 10:55:40 +0200

    Make processor work with inputs from memory

--- texttunnel/processor.py
+++ texttunnel/processor.py
@@ -40,66 +40,43 @@ Features:
 - Retries failed requests up to {max_attempts} times, to avoid missing data
 - Logs errors, to diagnose problems with requests
 
-Example command to call script:
-```
-python examples/api_request_parallel_processor.py \
-  --requests_filepath examples/data/example_requests_to_parallel_process.jsonl \
-  --save_filepath examples/data/example_requests_to_parallel_process_results.jsonl \
-  --request_url https://api.openai.com/v1/embeddings \
-  --max_requests_per_minute 1500 \
-  --max_tokens_per_minute 6250000 \
-  --token_encoding_name cl100k_base \
-  --max_attempts 5 \
-  --logging_level 20
-```
-
 Inputs:
-- requests_filepath : str
-    - path to the file containing the requests to be processed
-    - file should be a jsonl file, where each line is a json object with API parameters and an optional metadata field
-    - e.g., {"model": "text-embedding-ada-002", "input": "embed me", "metadata": {"row_id": 1}}
-    - as with all jsonl files, take care that newlines in the content are properly escaped (json.dumps does this automatically)
-    - an example file is provided at examples/data/example_requests_to_parallel_process.jsonl
-    - the code to generate the example file is appended to the bottom of this script
-- save_filepath : str, optional
+- requests: List[ChatCompletionRequest]
+    - the requests to process, see ChatCompletionRequest class for details
+- save_filepath: str, optional
     - path to the file where the results will be saved
     - file will be a jsonl file, where each line is an array with the original request plus the API response
     - e.g., [{"model": "text-embedding-ada-002", "input": "embed me"}, {...}]
     - if omitted, results will be saved to {requests_filename}_results.jsonl
-- request_url : str, optional
-    - URL of the API endpoint to call
-    - if omitted, will default to "https://api.openai.com/v1/embeddings"
-- api_key : str, optional
-    - API key to use
-    - if omitted, the script will attempt to read it from an environment variable {os.getenv("OPENAI_API_KEY")}
-- max_requests_per_minute : float, optional
-    - target number of requests to make per minute (will make less if limited by tokens)
-    - leave headroom by setting this to 50% or 75% of your limit
-    - if requests are limiting you, try batching multiple embeddings or completions into one request
-    - if omitted, will default to 1,500
-- max_tokens_per_minute : float, optional
-    - target number of tokens to use per minute (will use less if limited by requests)
-    - leave headroom by setting this to 50% or 75% of your limit
-    - if omitted, will default to 125,000
-- token_encoding_name : str, optional
-    - name of the token encoding used, as defined in the `tiktoken` package
-    - if omitted, will default to "cl100k_base" (used by `text-embedding-ada-002`)
-- max_attempts : int, optional
-    - number of times to retry a failed request before giving up
-    - if omitted, will default to 5
-- logging_level : int, optional
+- logging_level: int, optional
     - level of logging to use; higher numbers will log fewer messages
     - 40 = ERROR; will log only when requests fail after all retries
     - 30 = WARNING; will log when requests his rate limits or other errors
     - 20 = INFO; will log when requests start and the status at finish
     - 10 = DEBUG; will log various things as the loop runs to see when they occur
     - if omitted, will default to 20 (INFO).
+- max_attempts: int, optional
+    - number of times to retry a failed request before giving up
+    - if omitted, will default to 5
+- rate_limit_headroom_factor: float, optional
+    - factor to multiply the rate limit by to guarantee that the script stays under the limit
+    - if omitted, will default to 0.75 (75% of the rate limit)
+- token_encoding_name: str, optional
+    - name of the token encoding used, as defined in the `tiktoken` package
+    - if omitted, will default to "cl100k_base" (used by `text-embedding-ada-002`)
+- request_url: str, optional
+    - URL of the API endpoint to call
+    - if omitted, will default to "https://api.openai.com/v1/chat/completions"
+- api_key: str, optional
+    - API key to use
+    - if omitted, the function will attempt to read it from an environment variable {os.getenv("OPENAI_API_KEY")}
+
 
 The script is structured as follows:
     - Imports
-    - Define main()
+    - Define process_api_requests()
         - Initialize things
-        - In main loop:
+        - In process_api_requests loop:
             - Get next request if one is not already waiting for capacity
             - Update available token & request capacity
             - If enough capacity available, call API
@@ -113,7 +90,6 @@ The script is structured as follows:
         - append_to_jsonl (writes to results file)
         - num_tokens_consumed_from_request (bigger function to infer token usage from request)
         - task_id_generator_function (yields 1, 2, 3, ...)
-    - Run main()
 """
 
 # imports
@@ -121,25 +97,63 @@ import aiohttp  # for making API calls concurrently
 import asyncio  # for running API calls concurrently
 import json  # for saving results to a jsonl file
 import logging  # for logging rate limit warnings and other messages
+import os  # for reading API key from environment variable
+import sys  # for checking notebook vs. script
 import re  # for matching endpoint from request URL
 import tiktoken  # for counting tokens
+from typing import List, Optional  # for type hints
 import time  # for sleeping after rate limit is hit
 from dataclasses import (
     dataclass,
     field,
 )  # for storing API inputs, outputs, and metadata
 
+from texttunnel.chat import ChatCompletionRequest
 
-async def process_api_requests_from_file(
-    requests_filepath: str,
+
+def process_api_requests(
+    requests: List[ChatCompletionRequest],
     save_filepath: str,
-    request_url: str,
-    api_key: str,
-    max_requests_per_minute: float,
-    max_tokens_per_minute: float,
-    token_encoding_name: str,
-    max_attempts: int,
+    logging_level: int = 20,
+    max_attempts: int = 10,
+    rate_limit_headroom_factor: float = 0.75,
+    token_encoding_name: str = "cl100k_base",
+    request_url: str = "https://api.openai.com/v1/chat/completions",
+    api_key: Optional[str] = None,
+):
+    """
+    Processes API requests in parallel, throttling to stay under rate limits.
+    This function is a wrapper for aprocess_api_requests() that runs it in an asyncio event loop.
+    """
+    # Handle Notebook environment
+    if "ipykernel" in sys.modules:
+        import nest_asyncio
+
+        nest_asyncio.apply()
+
+    asyncio.run(
+        aprocess_api_requests(
+            requests,
+            save_filepath,
+            logging_level,
+            max_attempts,
+            rate_limit_headroom_factor,
+            token_encoding_name,
+            request_url,
+            api_key,
+        )
+    )
+
+
+async def aprocess_api_requests(
+    requests: List[ChatCompletionRequest],
+    save_filepath: str,
     logging_level: int,
+    max_attempts: int,
+    rate_limit_headroom_factor: float,
+    token_encoding_name: str,
+    request_url: str,
+    api_key: Optional[str] = None,
 ):
     """Processes API requests in parallel, throttling to stay under rate limits."""
     # constants
@@ -147,6 +161,23 @@ async def process_api_requests_from_file(
     seconds_to_sleep_each_loop = (
         0.001  # 1 ms limits max throughput to 1,000 requests per second
     )
+
+    # Check that all requests use the same model. Otherwise, we can't set
+    # a single rate limit for all requests.
+    if len(set([request.model.name for request in requests])) > 1:
+        raise ValueError("All requests must use the same model.")
+
+    if rate_limit_headroom_factor < 0.01 or rate_limit_headroom_factor > 1:
+        raise ValueError("rate_limit_headroom_factor must be between 0.01 and 1.")
+
+    if api_key is None:
+        try:
+            api_key = os.getenv("OPENAI_API_KEY")
+            assert api_key is not None
+        except AssertionError:
+            raise ValueError(
+                "api_key must be provided or set as an environment variable OPENAI_API_KEY."
+            )
 
     # initialize logging
     logging.basicConfig(level=logging_level)
@@ -167,6 +198,13 @@ async def process_api_requests_from_file(
     next_request = None  # variable to hold the next request to call
 
     # initialize available capacity counts
+    max_requests_per_minute = (
+        requests[0].model.requests_per_minute * rate_limit_headroom_factor
+    )
+    max_tokens_per_minute = (
+        requests[0].model.tokens_per_minute * rate_limit_headroom_factor
+    )
+
     available_request_capacity = max_requests_per_minute
     available_token_capacity = max_tokens_per_minute
     last_update_time = time.time()
@@ -175,116 +213,112 @@ async def process_api_requests_from_file(
     file_not_finished = True  # after file is empty, we'll skip reading it
     logging.debug("Initialization complete.")
 
-    # initialize file reading
-    with open(requests_filepath) as file:
-        # `requests` will provide requests one at a time
-        requests = file.__iter__()
-        logging.debug("File opened. Entering main loop")
+    # initialize requests reading
+    requests_queue = requests.copy()
+    logging.debug("Requests loaded. Entering main loop.")
 
-        while True:
-            # get next request (if one is not already waiting for capacity)
-            if next_request is None:
-                if not queue_of_requests_to_retry.empty():
-                    next_request = queue_of_requests_to_retry.get_nowait()
-                    logging.debug(
-                        f"Retrying request {next_request.task_id}: {next_request}"
-                    )
-                elif file_not_finished:
-                    try:
-                        # get new request
-                        request_json = json.loads(next(requests))
-                        next_request = APIRequest(
-                            task_id=next(task_id_generator),
-                            request_json=request_json,
-                            token_consumption=num_tokens_consumed_from_request(
-                                request_json, api_endpoint, token_encoding_name
-                            ),
-                            attempts_left=max_attempts,
-                            metadata=request_json.pop("metadata", None),
-                        )
-                        status_tracker.num_tasks_started += 1
-                        status_tracker.num_tasks_in_progress += 1
-                        logging.debug(
-                            f"Reading request {next_request.task_id}: {next_request}"
-                        )
-                    except StopIteration:
-                        # if file runs out, set flag to stop reading it
-                        logging.debug("Read file exhausted")
-                        file_not_finished = False
+    while True:
+        if len(requests_queue) > 0:
+            next_request_input = requests_queue.pop(0)
+        else:
+            logging.debug("Requests queue exhausted")
+            file_not_finished = False
 
-            # update available capacity
-            current_time = time.time()
-            seconds_since_update = current_time - last_update_time
-            available_request_capacity = min(
-                available_request_capacity
-                + max_requests_per_minute * seconds_since_update / 60.0,
-                max_requests_per_minute,
-            )
-            available_token_capacity = min(
-                available_token_capacity
-                + max_tokens_per_minute * seconds_since_update / 60.0,
-                max_tokens_per_minute,
-            )
-            last_update_time = current_time
-
-            # if enough capacity available, call API
-            if next_request:
-                next_request_tokens = next_request.token_consumption
-                if (
-                    available_request_capacity >= 1
-                    and available_token_capacity >= next_request_tokens
-                ):
-                    # update counters
-                    available_request_capacity -= 1
-                    available_token_capacity -= next_request_tokens
-                    next_request.attempts_left -= 1
-
-                    # call API
-                    asyncio.create_task(
-                        next_request.call_api(
-                            request_url=request_url,
-                            request_header=request_header,
-                            retry_queue=queue_of_requests_to_retry,
-                            save_filepath=save_filepath,
-                            status_tracker=status_tracker,
-                        )
-                    )
-                    next_request = None  # reset next_request to empty
-
-            # if all tasks are finished, break
-            if status_tracker.num_tasks_in_progress == 0:
-                break
-
-            # main loop sleeps briefly so concurrent tasks can run
-            await asyncio.sleep(seconds_to_sleep_each_loop)
-
-            # if a rate limit error was hit recently, pause to cool down
-            seconds_since_rate_limit_error = (
-                time.time() - status_tracker.time_of_last_rate_limit_error
-            )
-            if seconds_since_rate_limit_error < seconds_to_pause_after_rate_limit_error:
-                remaining_seconds_to_pause = (
-                    seconds_to_pause_after_rate_limit_error
-                    - seconds_since_rate_limit_error
+        # get next request (if one is not already waiting for capacity)
+        if next_request is None:
+            if not queue_of_requests_to_retry.empty():
+                next_request = queue_of_requests_to_retry.get_nowait()
+                logging.debug(
+                    f"Retrying request {next_request.task_id}: {next_request}"
                 )
-                await asyncio.sleep(remaining_seconds_to_pause)
-                # ^e.g., if pause is 15 seconds and final limit was hit 5 seconds ago
-                logging.warn(
-                    f"Pausing to cool down until {time.ctime(status_tracker.time_of_last_rate_limit_error + seconds_to_pause_after_rate_limit_error)}"
+            elif file_not_finished:
+                # get new request
+                request_json = next_request_input.to_dict()
+                next_request = APIRequest(
+                    task_id=next(task_id_generator),
+                    request_json=request_json,
+                    token_consumption=num_tokens_consumed_from_request(
+                        request_json, api_endpoint, token_encoding_name
+                    ),
+                    attempts_left=max_attempts,
+                    metadata=request_json.pop("metadata", None),
+                )
+                status_tracker.num_tasks_started += 1
+                status_tracker.num_tasks_in_progress += 1
+                logging.debug(
+                    f"Reading request {next_request.task_id}: {next_request}"
                 )
 
-        # after finishing, log final status
-        logging.info(
-            f"""Parallel processing complete. Results saved to {save_filepath}"""
+        # update available capacity
+        current_time = time.time()
+        seconds_since_update = current_time - last_update_time
+        available_request_capacity = min(
+            available_request_capacity
+            + max_requests_per_minute * seconds_since_update / 60.0,
+            max_requests_per_minute,
         )
-        if status_tracker.num_tasks_failed > 0:
-            logging.warning(
-                f"{status_tracker.num_tasks_failed} / {status_tracker.num_tasks_started} requests failed. Errors logged to {save_filepath}."
+        available_token_capacity = min(
+            available_token_capacity
+            + max_tokens_per_minute * seconds_since_update / 60.0,
+            max_tokens_per_minute,
+        )
+        last_update_time = current_time
+
+        # if enough capacity available, call API
+        if next_request:
+            next_request_tokens = next_request.token_consumption
+            if (
+                available_request_capacity >= 1
+                and available_token_capacity >= next_request_tokens
+            ):
+                # update counters
+                available_request_capacity -= 1
+                available_token_capacity -= next_request_tokens
+                next_request.attempts_left -= 1
+
+                # call API
+                asyncio.create_task(
+                    next_request.call_api(
+                        request_url=request_url,
+                        request_header=request_header,
+                        retry_queue=queue_of_requests_to_retry,
+                        save_filepath=save_filepath,
+                        status_tracker=status_tracker,
+                    )
+                )
+                next_request = None  # reset next_request to empty
+
+        # if all tasks are finished, break
+        if status_tracker.num_tasks_in_progress == 0:
+            break
+
+        # main loop sleeps briefly so concurrent tasks can run
+        await asyncio.sleep(seconds_to_sleep_each_loop)
+
+        # if a rate limit error was hit recently, pause to cool down
+        seconds_since_rate_limit_error = (
+            time.time() - status_tracker.time_of_last_rate_limit_error
+        )
+        if seconds_since_rate_limit_error < seconds_to_pause_after_rate_limit_error:
+            remaining_seconds_to_pause = (
+                seconds_to_pause_after_rate_limit_error - seconds_since_rate_limit_error
             )
-        if status_tracker.num_rate_limit_errors > 0:
-            logging.warning(
-                f"{status_tracker.num_rate_limit_errors} rate limit errors received. Consider running at a lower rate."
+            await asyncio.sleep(remaining_seconds_to_pause)
+            # ^e.g., if pause is 15 seconds and final limit was hit 5 seconds ago
+            logging.warn(
+                f"Pausing to cool down until {time.ctime(status_tracker.time_of_last_rate_limit_error + seconds_to_pause_after_rate_limit_error)}"
             )
+
+    # after finishing, log final status
+    logging.info(f"""Parallel processing complete. Results saved to {save_filepath}""")
+    if status_tracker.num_tasks_failed > 0:
+        logging.warning(
+            f"{status_tracker.num_tasks_failed} / {status_tracker.num_tasks_started} requests failed. Errors logged to {save_filepath}."
+        )
+    if status_tracker.num_rate_limit_errors > 0:
+        logging.warning(
+            f"{status_tracker.num_rate_limit_errors} rate limit errors received. Consider running at a lower rate."
+        )
 
 
 # dataclasses
