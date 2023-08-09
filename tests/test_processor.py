9e7e7ffa4db102ba75edfca5a207de90222c4cd7
Paul Simmering <paul.simmering@gmail.com>
2023-08-09 18:21:18 +0200

    Add test for prepare_output_filepath

---
+++ tests/test_processor.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from texttunnel import processor
+
+def test_prepare_output_filepath_tempfile():
+    path = processor.prepare_output_filepath(None, keep_file=False)
+    try:
+        assert isinstance(path, Path)
+        assert path.exists()
+    finally:
+        path.unlink()
