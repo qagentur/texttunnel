c5774b92140cfa9de962fdcc5c0ce65e57ea65be
Paul Simmering <paul.simmering@gmail.com>
2023-08-09 18:22:11 +0200

    Format

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from texttunnel import processor
 
+
 def test_prepare_output_filepath_tempfile():
     path = processor.prepare_output_filepath(None, keep_file=False)
     try:
